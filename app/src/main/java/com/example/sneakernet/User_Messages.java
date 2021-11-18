eb5102d9ecad3e59968605ef869bcfe7f2c5e982
gedeh <gedeh@iastate.edu>
2021-11-18 15:45:13 -0600

    skeleton that will be used for code checkpoint

---
+++ app/src/main/java/com/example/sneakernet/User_Messages.java
@@ -0,0 +1,14 @@
+package com.example.sneakernet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_Messages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages);
+    }
+}
